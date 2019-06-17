7ddd50fdb94fce8ea5327fcdf243a22724fe5dea
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-17 16:57:21 +0530

    Learning Python now

--- stackoverflow.py
+++ stackoverflow.py
@@ -1,4 +1,2 @@
-import requests
-request = requests.get("https://drive.google.com/uc?export=download&id=1_9On2-nsBQIw3JiY43sWbrF8EjrqrR4U")
-with open(developer_survey_2018.zip, "wb") as file:
-    file.write(request.con)
+message = """Meet me tonight."""
+print(message)
