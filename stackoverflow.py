b65edc416710be5fbca325aac82e0ed52d933823
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-18 06:31:19 +0530

    if statement

--- stackoverflow.py
+++ stackoverflow.py
@@ -1,2 +1,6 @@
-message = """Meet me tonight."""
-print(type(3 + 5.7j))
+import sys
+text_input = input("Please enter a test string: ")
+
+if len(text_input) < 6:
+    print("Your string is too short")
+    print("it should be at least 6 characters long")
