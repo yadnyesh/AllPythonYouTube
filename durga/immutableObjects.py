7ade9d5168db0de487b62d499e03148e8d77f940
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-08-18 20:01:16 +0530

    list

--- durga/immutableObjects.py
+++ durga/immutableObjects.py
@@ -7,8 +7,9 @@ print(id(x))
 # 4351483856
 # 4351483888
 
-myList = [10,20,30]
+myList = [10,20,30, 'yadnyesh','great','awesome']
 print(id(myList))
 myList[0] = 7777
 print(id(myList))
 print(myList)
+print(myList[1:4])
