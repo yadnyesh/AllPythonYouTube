a2a8d32cf7b53bbd71bc858e83650b4923edcec6
yadnyesh <yadnyesh.juvekar@gmail.com>
2020-08-28 21:08:05 +0530

    Logical Operators

--- durga/dict.py
+++ durga/dict.py
@@ -1,4 +1,7 @@
-d = { 7:'Yadnyesh',8:'Bindiya', 9:'Varada'}
+d = { 7:'Yadnyesh',
+      8:'Bindiya', 
+      9:'Varada'
+    }
 d[10] = 'New Member'
 d['one'] = 'One'
 r = range(10, 20, 2)
---
+++ durga/logicalOp.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+
+print ( a and b)
+print ([] and b)
+print (() and b)
+print ({} and b)
+print ( a or b)
