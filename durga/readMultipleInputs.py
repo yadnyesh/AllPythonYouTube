a34970064fdf7c843810e9e72ecc1a6c7ee2781c
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-09-14 09:51:49 +0530

    print multiple float values

--- durga/readMultipleInputs.py
+++ durga/readMultipleInputs.py
@@ -1,2 +1,4 @@
-first, last = [int(x) for x in input('Enter 2 numbers: ').split(',')]
+first, last = [int(x) for x in input('Enter 2 numbers: ').split()]
 print('The sum of these numbers is: ', (first + last))
+float1, float2, float3 = [float(x) for(x) in input('Enter 3 float numbers: ').split(',')]
+print('The sum of the three numbers is: ', float1+float2+float3)
