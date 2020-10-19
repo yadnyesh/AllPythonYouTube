c29dfb472e7e4de3e3eba82dacc1e69acd590e09
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-10-19 09:43:07 +0530

    Example of ElseIF

---
+++ durga/section6/numberToWords.py
@@ -0,0 +1,13 @@
+n = int(input('Enter digit between 0 and 4: '))
+if n == 0:
+    print('Zero')
+elif n == 1:
+    print('One')
+elif n == 2:
+    print('Two')    
+elif n == 3:
+    print('Three')
+elif n ==4:
+    print('Four')
+else:
+    print('Invalid Input')          
