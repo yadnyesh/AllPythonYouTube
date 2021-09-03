d857930ea3b43e654b118c18b094b2003049d970
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-09-03 18:19:35 +0530

    repetetive code

--- nana/main.py
+++ nana/main.py
@@ -1,1 +1,7 @@
-print(f"20 days are {20 * 24 * 60} minutes")
+number_of_hours_in_a_day = 24
+name_of_unit = " hours "
+
+print(f"20 days are {20 * 24} {name_of_unit}")
+print(f"20 days are {30 * 24} {name_of_unit}")
+print(f"20 days are {40 * 24} {name_of_unit}")
+print(f"20 days are {50 * 24} {name_of_unit}")
