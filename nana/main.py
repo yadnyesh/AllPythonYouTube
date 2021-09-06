9bbde250ccbc87bb305042a791ed1da3efca013c
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-09-06 17:00:26 +0530

    List Program working

--- nana/main.py
+++ nana/main.py
@@ -1,6 +1,6 @@
 number_of_hours_in_a_day = 24
 name_of_unit = " hours "
-ip_number_of_days = 0
+
 
 def days_to_units(number_of_days):
     return f"{number_of_days} days are {number_of_days * 24} {name_of_unit}"
@@ -8,16 +8,19 @@ def days_to_units(number_of_days):
 
 def validate_and_execute():
     try:
-        if (ip_number_of_days > 0):
-            calculated_value = days_to_units(ip_number_of_days)
+        ip_number_of_days_int = int(number_of_days)
+        if (ip_number_of_days_int > 0):
+            calculated_value = days_to_units(ip_number_of_days_int)
             print(calculated_value)
-        elif ip_number_of_days == 0:
+        elif ip_number_of_days_int == 0:
             print("Number of days should not be 0")
         else:
             print("You entered a -ve number, please don't do that!")    
     except ValueError:
         print("Number of days should be a number and > 1")
 
-while True:
-    ip_number_of_days = int(input("Enter the number of days: " ))        
-    validate_and_execute()
+ip_number_of_days = ""
+while ip_number_of_days != "exit":
+    ip_number_of_days = input("Enter the number of days: " )
+    for number_of_days in ip_number_of_days.split(","):    
+        validate_and_execute()
