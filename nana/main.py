e02d93eaf5039fbba3b1770cd8368549e60a3df3
Yadnyesh <yadnyesh.juvekar@walmart.com>
2021-09-06 13:04:50 +0530

    good one python :)

--- nana/main.py
+++ nana/main.py
@@ -2,10 +2,12 @@ number_of_hours_in_a_day = 24
 name_of_unit = " hours "
 
 def days_to_units(number_of_days):
-    return(f"{number_of_days} days are {number_of_days * 24} {name_of_unit}")
-
-ip_number_of_days = int(input("Enter the number of days: 10" ))
-calculated_value = days_to_units(10)
+    if (number_of_days > 0):
+        return(f"{number_of_days} days are {number_of_days * 24} {name_of_unit}")
+    else:
+        return("Number of days should be a number and > 1")
+ip_number_of_days = int(input("Enter the number of days: " ))
+calculated_value = days_to_units(ip_number_of_days)
 print(calculated_value)
 days_to_units(ip_number_of_days)
 
