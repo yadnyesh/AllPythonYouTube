cb5b968e1b0d2ee76b47c74b42c4bc610b97daae
Yadnyesh <yadnyesh.juvekar@walmart.com>
2021-09-06 14:07:19 +0530

    tried debug ....wow

--- nana/main.py
+++ nana/main.py
@@ -6,8 +6,12 @@ def days_to_units(number_of_days):
         return(f"{number_of_days} days are {number_of_days * 24} {name_of_unit}")
     else:
         return("Number of days should be a number and > 1")
-ip_number_of_days = int(input("Enter the number of days: " ))
-calculated_value = days_to_units(ip_number_of_days)
-print(calculated_value)
-days_to_units(ip_number_of_days)
 
+ip_number_of_days = input("Enter the number of days: " )
+
+if ip_number_of_days.isdigit():
+    ip_number_of_days = int(ip_number_of_days)
+    calculated_value = days_to_units(ip_number_of_days)
+    print(calculated_value)
+else:
+    print("Number of days should be a number and > 1")
